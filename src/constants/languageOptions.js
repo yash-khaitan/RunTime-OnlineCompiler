8031640f0eb2d5247306926398107eb9e8ce29ae
Yash Khaitan <yashkhaitan107@gmail.com>
2023-08-19 20:37:46 +0530

    Remove C++ language support

--- src/constants/languageOptions.js
+++ src/constants/languageOptions.js
@@ -1,25 +1,24 @@
 export const languageOptions = [
   {
     id: 71,
-    name: 'Python (3.8.1)',
-    label: 'Python (3.8.1)',
-    value: 'python',
-    defaultCode: '# Write code'
+    name: "Python (3.8.1)",
+    label: "Python (3.8.1)",
+    value: "python",
+    defaultCode: "# Write code below",
   },
-  {
-    id: 54,
-    name: 'C++ (GCC 9.2.0)',
-    label: 'C++ (GCC 9.2.0)',
-    value: 'cpp',
-    defaultCode: '// Write code'
+  // {
+  //   id: 54,
+  //   name: 'C++ (GCC 9.2.0)',
+  //   label: 'C++ (GCC 9.2.0)',
+  //   value: 'cpp',
+  //   defaultCode: '// Write code'
 
-  },
+  // },
   {
     id: 62,
-    name: 'Java (OpenJDK 13.0.1)',
-    label: 'Java (OpenJDK 13.0.1)',
-    value: 'java',
-    defaultCode: '// Write code'
-
+    name: "Java (OpenJDK 13.0.1)",
+    label: "Java (OpenJDK 13.0.1)",
+    value: "java",
+    defaultCode: "// Write code below. name your class as 'Main' ",
   },
 ];
